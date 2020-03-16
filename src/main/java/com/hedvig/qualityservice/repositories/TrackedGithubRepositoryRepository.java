ab14600be30ad71fefa5a1b704e5340dc7294242
Johan Palmfjord <johan.palmfjord@hedvig.com>
2020-03-16 11:03:53 +0100

    Add missing @Repository annotation

--- src/main/java/com/hedvig/qualityservice/repositories/TrackedGithubRepositoryRepository.java
+++ src/main/java/com/hedvig/qualityservice/repositories/TrackedGithubRepositoryRepository.java
@@ -2,8 +2,10 @@ package com.hedvig.qualityservice.repositories;
 
 import com.hedvig.qualityservice.entities.TrackedGithubRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TrackedGithubRepositoryRepository extends CrudRepository<TrackedGithubRepository, UUID> {
 }
